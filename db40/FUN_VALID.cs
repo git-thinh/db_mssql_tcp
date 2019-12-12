83dbb212dbcc5c623bb384d0bc39794196178eb7
nguyenvanthinh <nguyenvanthinh@f88.vn>
2019-12-12 10:18:58 +0700

    a

--- db40/FUN_VALID.cs
+++ db40/FUN_VALID.cs
@@ -20,9 +20,51 @@ namespace db40
             if (value == null) return false;
 
             bool ok = Is_Numeric(value);
-            if (ok == false) return ok;
+            if (ok) {
+                string s = value.ToString();
+                if (s.Length != 8) return false;
 
+                if (s[0] == '0' || s.StartsWith("0000")) return false;
+
+                int m = int.Parse(s.Substring(4, 2));
+                if (m > 12 || m == 0) return false;
+
+                int d = int.Parse(s.Substring(6, 2));
+                if (d > 31 || d == 0) return false;
+
+                return true;
+            }
+
+            return false;
         }
+
+        public static bool Is_Numeric_HHmmss(object value)
+        {
+            if (value == null) return false;
+
+            bool ok = Is_Numeric(value);
+            if (ok) {
+                string s = value.ToString();
+                if (s.Length == 5) s = "0" + s;
+                if (s.Length != 6) return false;
+
+                int h = int.Parse(s.Substring(0, 2));
+                if (h > 12) return false;
+
+                int m = int.Parse(s.Substring(2, 2));
+                if (m > 60) return false;
+
+                int se = int.Parse(s.Substring(4, 2));
+                if (se > 60) return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+
+
 
 
     }
